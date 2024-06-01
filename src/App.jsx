d980c761852453b6d40b305104dea0e6ee913d62
Lcdiego <diegocolman78@hotmail.com>
2024-06-01 10:53:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+<div className="bg-gray-200 text-green-500 text-9xl h-[100vh] flex justify-center items-center">
+  Hola grupo
+</div>
+  )
+}
+
+export default App
