9e78a81d16fb8ddbb939b49aa283d6f4f76c7b23
Federico Sabiche <fsabiche>
2024-06-20 17:37:01 -0300

    se agrego seccion tarjetas de pizzas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Home from "./components/Home/Home.jsx";
+import TarjetasPizzas from "./Pages/TarjetasPizzas.jsx";
 
 function App() {
   return (
@@ -8,9 +9,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Tarjetaspizzas" element={<TarjetasPizzas />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
---
+++ src/Pages/TarjetasPizzas.jsx
@@ -0,0 +1,365 @@
+import pizza_1 from "../assets/pizza.png";
+import pizza_2 from "../assets/pizza_2.png";
+import pizza_3 from "../assets/pizza_3.png";
+import pizza_4 from "../assets/pizza_4.png";
+import pizza_5 from "../assets/pizza_5.png";
+
+const TarjetasPizzas = () => {
+  return (
+    <>
+      <div className="bg-amber-400">
+        <div id="container" className="w-4/5 mx-auto">
+          <div className="flex flex-col sm:flex-row">
+            {/*Inicio Tarjeta 1 */}
+            <div className="sm:w-1/4 p-2 mt-32">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_1}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Inicio Tarjeta 2 */}
+            <div className="sm:w-1/4 p-2 mt-32">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_2}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Inicio Tarjeta 3 */}
+            <div className="sm:w-1/4 p-2 mt-32">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_3}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Inicio Tarjeta 4 */}
+            <div className="sm:w-1/4 p-2 mt-32">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_4}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Fin trajeta 4 */}
+            {/*Inicio Tarjeta 4 */}
+            <div className="sm:w-1/4 p-2 mt-32">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_5}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Fin trajeta 4 */}
+          </div>
+          <div className="flex flex-col sm:flex-row mt-20">
+            {/*Inicio Tarjeta 1 */}
+            <div className="sm:w-1/4 p-2">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_1}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Inicio Tarjeta 2 */}
+            <div className="sm:w-1/4 p-2">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_2}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Inicio Tarjeta 3 */}
+            <div className="sm:w-1/4 p-2">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_3}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Inicio Tarjeta 4 */}
+            <div className="sm:w-1/4 p-2">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_5}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Fin trajeta 4 */}
+            {/*Inicio Tarjeta 4 */}
+            <div className="sm:w-1/4 p-2">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_4}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Fin trajeta 4 */}
+          </div>
+          <div className="flex flex-col sm:flex-row mt-20">
+            {/*Inicio Tarjeta 1 */}
+            <div className="sm:w-1/4 p-2">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_3}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Inicio Tarjeta 2 */}
+            <div className="sm:w-1/4 p-2">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_2}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Inicio Tarjeta 3 */}
+            <div className="sm:w-1/4 p-2">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_5}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Inicio Tarjeta 4 */}
+            <div className="sm:w-1/4 p-2">
+              <div className="bg-amber-300 px-6 py-8 rounded-lg shadow-lg text-center">
+                <div className="mb-3">
+                  <img
+                    className="w-auto mx-auto rounded-full -mt-20"
+                    src={pizza_4}
+                    alt=""
+                  />
+                </div>
+                <h2 className="text-xl font-medium text-black">Muzzarella</h2>
+                <span className="text-black block mb-5">
+                  Salsa de tomate, queso muzzarella y orégano.
+                </span>
+                <div className="flex justify-end w-full">
+                  <button
+                    href="#"
+                    className="px-6 py-2 border-2 border-red-500 text-red-500 rounded-full"
+                  >
+                    +Info
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*Fin trajeta 4 */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TarjetasPizzas;
