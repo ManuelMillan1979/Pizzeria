5576777d53108496b085fb3283758ab02d31ee48
Federico Sabiche <fsabiche>
2024-06-21 17:58:30 -0300

    se agrego nuevo lote de empanadas

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import TarjetasPizzas from "./Pages/TarjetasPizzas.jsx";
 import TarjetasEmpanadas from './Pages/TarjetasEmpanadas.jsx';
 import TarjetasBebidas from './Pages/TarjetasBebidas.jsx';
 import EmpanadaCarne100 from './Pages/TarjetasEmpanadas/EmpanadaCarne100.jsx';
-import EmpanadaCalabaza from './Pages/TarjetasEmpanadas/empanadaCalabaza.jsx';
+import EmpanadaCalabaza from './Pages/TarjetasEmpanadas/EmpanadaCalabaza.jsx';
 import EmpanadaPorkAgri from './Pages/TarjetasEmpanadas/EmpanadaPorkAgri.jsx';
+import EmpanadaFullPanceta from './Pages/TarjetasEmpanadas/EmpanadaFullPanceta.jsx';
+import NotFound from './Pages/404NotFound.jsx'; // Create a NotFound component
+import EmpanadaPolloBBQ from './Pages/TarjetasEmpanadas/EmpanadaPolloBBQ.jsx';
 
 function App() {
   return (
@@ -14,16 +17,18 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Tarjetaspizzas" element={<TarjetasPizzas />} />
-        <Route path='/TarjetasEmpanadas' element={<TarjetasEmpanadas/>} />
-        <Route path='/tarjetasbebidas' element={<TarjetasBebidas/>} />
-        <Route path='/empanadaCarne100' element={<EmpanadaCarne100/>} />
-        <Route path='/empanadaCalabaza' element={<EmpanadaCalabaza />} />
-        <Route path='/empanadaPorkAgri' element={<EmpanadaPorkAgri />} />
+        <Route path="/tarjetasPizzas" element={<TarjetasPizzas />} />
+        <Route path="/tarjetasEmpanadas" element={<TarjetasEmpanadas />} />
+        <Route path="/tarjetasBebidas" element={<TarjetasBebidas />} />
+        <Route path="/empanadaCarne100" element={<EmpanadaCarne100 />} />
+        <Route path="/empanadaCalabaza" element={<EmpanadaCalabaza />} />
+        <Route path="/empanadaPorkAgri" element={<EmpanadaPorkAgri />} />
+        <Route path="/empanadaFullPanceta" element={<EmpanadaFullPanceta />} />
+        <Route path='/empanadaPolloBBQ' element={<EmpanadaPolloBBQ />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
